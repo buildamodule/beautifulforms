d90ed31848b89bfb97d1bf03aa03bac7e2746794
Chris Shattuck <hello@buildamodule.com>
2011-10-09 19:40:13 -0600

    Reorganized modules, added LESS dependencies

--- sites/all/modules/custom/beautifulforms/beauty02infield/beauty02infield.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  Drupal.behaviors.beauty02infield = {
-    attach: function (context, settings) {
-      // Add code here.
-      $("label").inFieldLabels();
-    }
-  }
-})(jQuery);
